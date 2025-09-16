7d7ba0fca908baf462a1c96d67a682848b121de1
tastycheddar <dave@saltandscoville.com>
2025-09-16 17:03:13 -0600

    Add client/src/components/admin/blog/BlogCategories.tsx

---
+++ client/src/components/admin/blog/BlogCategories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CategoryManager from './CategoryManager';
+
+const BlogCategories: React.FC = () => {
+  return <CategoryManager />;
+};
+
+export default BlogCategories;
